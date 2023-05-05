8ec88a8d3d1632e5e4c5f631dc44e9233693f228
Bryan <bryan.oliveira12345@gmail.com>
2023-05-05 11:00:11 -0300

    Created new fields in contact models and added the model in admin.py

--- contact/models.py
+++ contact/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.utils import timezone
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    phone = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50, verbose_name='Nome')
+    last_name = models.CharField(max_length=50, verbose_name='Sobrenome')
+    phone = models.CharField(max_length=50, verbose_name='Telefone')
     email = models.EmailField(max_length=254, blank=True)
-    created_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(default=timezone.now, verbose_name='Data de Criação')
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     
+    def __str__(self) -> str:
+        return f"{self.first_name}"
