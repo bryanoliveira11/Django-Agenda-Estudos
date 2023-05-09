ce5eb8491b64ccf6f5d13fddc4052f10ad0041a1
Bryan <bryan.oliveira12345@gmail.com>
2023-05-09 10:58:02 -0300

    created contact view and html to display a single contact

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
 
 
@@ -7,6 +7,21 @@ def index(request):
 
     context = {
         'contacts': contacts,
-    }
+        'site_title': "Contatos |"
+        }
 
     return render(request, 'contact/index.html', context)
+
+
+def contact(request, contact_id):
+    single_contact = get_object_or_404(
+        Contact.objects.filter(pk=contact_id, show=True))
+
+    contact_name = f"{single_contact.first_name} {single_contact.last_name}"
+
+    context = {
+        'contact': single_contact,
+        'site_title': f" {contact_name} |"
+    }
+
+    return render(request, 'contact/contact.html', context)
