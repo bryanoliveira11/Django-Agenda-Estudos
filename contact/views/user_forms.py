fa7505b82885b9e231bd5eab1e1f0c9eff97e303
Bryan <bryan.oliveira12345@gmail.com>
2023-05-12 12:02:17 -0300

    login and logout views

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from contact.forms import RegisterForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
+from contact.forms import RegisterForm, RegisterUpdateForm
 
 
 def register(request):
@@ -10,9 +13,52 @@ def register(request):
 
         if form.is_valid():
             form.save()
+            messages.success(request, 'Usuário Registrado ! ')
+            return redirect('contact:index')
 
     context = {
-        'form': form
+        'form': form,
     }
 
     return render(request, 'contact/register.html', context)
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'Login Feito com Sucesso ! ')
+            return redirect('contact:index')
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'contact/login.html', context)
+
+
+def logout(request):
+    messages.success(request, 'Logout Efetuado ! ')
+    auth.logout(request)
+    return redirect('contact:login')
+
+
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+
+    if request.method != 'POST':
+        return render(request, 'contact/user_update.html', {'form': form})
+
+    form = RegisterUpdateForm(data=request.POST, instance=request.user)
+
+    if not form.is_valid():
+        return render(request, 'contact/user_update.html', {'form': form})
+
+    form.save()
+    messages.success(request, 'Dados Alterados com Sucesso ! ')
+    return redirect('contact:user_update')
